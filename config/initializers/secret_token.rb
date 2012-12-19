89f578cfeb0435bcbb03a0cc0bdab5a0c53051fe
Michael Nikitochkin <miry.sof@gmail.com>
2012-12-19 13:14:38 +0200

    Init a simple rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacebookMultipleLikeSample::Application.config.secret_token = '19e8b6270f30b98ac7df5e2d342ad7d8c3567f06459d3c43f46e865c8a3257e9bc874c75d1c1c3540a0af81196e0b25cb124df4a7ee7afb60228ed9b2227d81e'
